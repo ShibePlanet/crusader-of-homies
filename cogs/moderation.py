06cf83dbc11eb3ccf7c35033e8201be2d1aa3a91
ShibePlanet <alelo2006@live.com>
2021-06-08 16:39:18 -0400

    Fucking updates

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -1,6 +1,6 @@
 import discord
 from discord.ext import commands
-from discord.ext.commands import has_permissions, MisssingPermissions
+from discord.ext.commands import has_permissions
 from discord.ext.commands.errors import MissingPermissions
 
 
