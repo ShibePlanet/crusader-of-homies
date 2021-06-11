2780ee15b9eadf0c7cfdc3793620f2a74c0fe201
ShibePlanet <lelo2006@live.com>
2021-06-10 23:03:00 -0400

    Add furry gulag

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -30,7 +30,6 @@ class Mod(commands.Cog):
 
             await ctx.send(f'{user.mention} was sent to gbj.')
             await channel.send(f'{user.mention}, you have been sent to Gay Baby Jail. Please use this format to return to society:\n`I am sorry for (reason).`')
-
     @gbj.error
     async def gbj_error(self, ctx, error):
         if isinstance(error, MissingPermissions):
@@ -54,11 +53,59 @@ class Mod(commands.Cog):
         else: 
             await ctx.send('That person doesn\'t have that role. Do you mean `c!gbj (member)`?')
             return 
-
     @ungbj.error
     async def gbj_error(self, ctx, error):
         if isinstance(error, MissingPermissions):
             await ctx.send('Sorry, you don\'t have the permissions to do that.')
+
+
+    # FG
+    @commands.command()
+    @commands.has_permissions(kick_members = True)
+    async def fg(self, ctx, user: discord.Member = None):
+        if user == None:
+            await ctx.send('Please add a member. For more help, use `c!help`.')
+            return 
+        
+        role = discord.utils.get(user.guild.roles, name='furry gulag')
+        channel = discord.utils.get(user.guild.channels, name = 'furry-gulag')
+
+        if role in user.roles:
+            await ctx.send('That person already has that role. Do you mean `c!unfg (member)`?')
+            return 
+        else: 
+            await user.add_roles(role)
+
+            await ctx.send(f'{user.mention} was sent to the furry gulag.')
+            await channel.send(f'{user.mention}, you\'re a furry. How does it feel? DM staff if you have an issue.')
+    @fg.error
+    async def gbj_error(self, ctx, error):
+        if isinstance(error, MissingPermissions):
+            await ctx.send('Sorry, you don\'t have the permissions to do that.')
+
+
+    # UNFG
+    @commands.command()
+    @commands.has_permissions(kick_members = True)
+    async def unfg(self, ctx, user: discord.Member = None):
+        if user == None:
+            await ctx.send('Please add a member. For more help, use `c!help`.')
+            return 
+        
+        role = discord.utils.get(user.guild.roles, name='furry gulag')
+        channel = discord.utils.get(user.guild.channels, name = 'furry-gulag')
+
+        if role in user.roles:
+            await user.remove_roles(role)
+            await ctx.send(f'{user.mention} was removed from the furry gulag.')
+        else: 
+            await ctx.send('That person doesn\'t have that role. Do you mean `c!fg (member)`?')
+            return 
+    @unfg.error
+    async def gbj_error(self, ctx, error):
+        if isinstance(error, MissingPermissions):
+            await ctx.send('Sorry, you don\'t have the permissions to do that.')
+
 
 # Setup
 def setup(client):
